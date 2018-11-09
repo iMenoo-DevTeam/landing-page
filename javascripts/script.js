d29239c485ca3aca4aa5f93fcc394a1a55a16d96
Manuel <manueldelpinolucena@gmail.com>
2018-11-09 10:59:57 +0100

    added post email

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,22 +2,23 @@ window.onload = () => {
   sendInfo = (e, elem) => {
     e.preventDefault();
     const email = elem.parentNode.children[0].value;
-    // axios.post('https://imenoo-email-microservice.herokuapp.com/api/email', {email})
-    // .then(res => {
-    //   console.log(res)
-    // })
+    axios.post('https://imenoo-email-microservice.herokuapp.com/api/emai', { email })
+      .then(() => {
+        setTimeout(() => {
+          elem.classList.remove('sending');
+          elem.classList.add('send');
+          elem.innerHTML = "¡Gracias por tu interes. Pronto nos pondremos en contacto contigo!";
+        }, 2000)
+      })
+      .catch(() => {
+        setTimeout(() => {
+          elem.classList.remove('sending');
+          elem.classList.add('error');
+          elem.innerHTML = "¡Tuvimos un error procesando el envío. Intentalo más tarde!";
+        }, 2000)
+      });
     elem.classList.add('sending');
     elem.innerHTML = "Enviando...";
-    setTimeout(() => {
-      elem.classList.remove('sending');
-      elem.classList.add('send');
-      elem.innerHTML = "¡Gracias por tu interes. Pronto nos pondremos en contacto contigo!";
-    }, 6000)
-    setTimeout(() => {
-      elem.classList.remove('sending');
-      elem.classList.add('error');
-      elem.innerHTML = "¡Tuvimos un error procesando el envío. Intentalo más tarde!";
-    }, 10000)
   }
   
   $('.carousel').carousel();
